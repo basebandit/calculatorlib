b39e739072777de4edfeba1213624969afe0c8af
John Doe <john@doe.com>
2023-03-27 20:27:23 +0300

    add unit tests for calculator

---
+++ calculator_test.py
@@ -0,0 +1,15 @@
+"""
+Unit tests for the calculator library
+"""
+
+import calculator
+
+
+class TestCalculator:
+    def test_add(self):
+        ans = calculator.add(2, 2)
+        assert ans == 4
+
+    def test_subtract(self):
+        ans = calculator.minus(4, 2)
+        assert ans == 2
